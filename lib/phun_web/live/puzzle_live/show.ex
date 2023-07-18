800f26b86d5f22d88613246d7548385cc2d90b35
Nathan Hessler <xmilestegx@gmail.com>
2023-07-18 15:16:21 -0500

    add pubsub to the show page admin puzzles

--- lib/phun_web/live/puzzle_live/show.ex
+++ lib/phun_web/live/puzzle_live/show.ex
@@ -5,6 +5,7 @@ defmodule PhunWeb.PuzzleLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
+    Game.subscibe_puzzle_changed()
     {:ok, socket}
   end
 
@@ -13,10 +14,21 @@ defmodule PhunWeb.PuzzleLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:puzzle, Game.get_puzzle!(id))}
+     |> update_puzzle_points(id)}
   end
 
+  defp update_puzzle_points(socket, id) do
+    assign(socket, :puzzle, Game.get_puzzle!(id))
+  end
+     
   defp page_title(:show), do: "Show Puzzle"
   defp page_title(:edit), do: "Edit Puzzle"
   defp page_title(:points), do: "Edit Points"
+
+  @impl true
+  def handle_info({:points_changed, puzzle_id}, %{assigns: %{puzzle: %{id: puzzle_id}}}=socket) do
+    {:noreply, update_puzzle_points(socket, puzzle_id)}
+  end
+
+  def handle_info({:points_changed, _puzzle_id}, socket), do: {:noreply, socket}     
 end
